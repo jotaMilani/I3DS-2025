ecf8e60f6a1d6050ae8c4f57b51c0f13f57fc08c
milani <fandangoslord2@gmail.com>
2025-03-11 14:52:44 -0300

    melhoria

---
+++ devflix/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import './Footer.css'
+
+const Footer = (props) => {
+  return (
+    <footer>
+    <p>
+      Feito com ❤️ por <a href={props.devLink}>{props.devName}</a>
+    </p>
+  </footer>
+  )
+}
+
+export default Footer
