c2e3d87fb20c38af70bd2508a9224181ce233796
milani <fandangoslord2@gmail.com>
2025-03-12 17:10:14 -0300

    adicionando imagem

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/header/header";
+import Perfil from "./components/img/romero.gif"
 const App = () => {
   return (
       <div id="App">
@@ -12,9 +13,7 @@ const App = () => {
         <div class="geral">
         <div class="card-client">
           <div class="user-picture">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-              <path d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"></path>
-            </svg>
+        <img src="./components/img/romero.gif" alt="" />
           </div>
           <p class="name-client">
             {" "}
