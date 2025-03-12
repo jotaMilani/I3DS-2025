41c1f58fc6b30ebabc362138847c2963b7a4510f
milani <fandangoslord2@gmail.com>
2025-03-12 15:27:03 -0300

    adicionando components do projeto

---
+++ projeto/src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+    <button> Sobre mim</button>
+    <button>Projetos</button>
+    <button>Contato</button>
+    </div>
+  )
+}
+
+export default Header
